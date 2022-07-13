5d2f400354aae3aa01c41b919c6e8858aa2fc855
richard123gh <106679304+richard123gh@users.noreply.github.com>
2022-07-13 15:38:04 -0600

    initial table setup

--- dbtables/companyemployees.sql
+++ dbtables/companyemployees.sql
@@ -1,6 +1,17 @@
-CREATE TABLE employees (
-	id serial PRIMARY KEY, 
-	employee_name, 
-	username, 
-	employee_role
+--drop table if exists employees;
+
+CREATE TABLE employees(
+	id serial, 
+	employee_name varchar(50), 
+	username varchar(20) primary key, 
+	employee_role varchar(10)
 );
+
+insert into employees values(default, 'St. John Smythe', 'shakennotstirred', 'Manager');
+insert into employees values(default, 'Peter Venkman', 'themagicword', 'Requester');
+insert into employees values(default, 'Marcus Auraleus', 'themeditationking', 'Requester');
+insert into employees values(default, 'William Riker', 'maneuver', 'Requester');
+insert into employees values(default, 'Cotton McKnight', 'dodgeball', 'Manager');
+insert into employees values(default, 'Clarance Beeks', 'porkbellies', 'Requester');
+
+select * from employees;
