7956075e875800ff633f8daa36835fb18da560b3
quyen513 <quyen513@revature.net>
2022-07-27 19:46:50 -0400

    Close to be done

--- project2/src/test/java/com/project2/steps/LoginSteps.java
+++ project2/src/test/java/com/project2/steps/LoginSteps.java
@@ -24,7 +24,7 @@ public class LoginSteps {
     @Given("the manager is on the login page")
     public void the_manager_is_on_the_login_page(){
         
-        TestRunner.driver.get("File://C:/Users/aflem/OneDrive/Desktop/Project2/project2/src/main/resources/web-pages/login.html");
+        TestRunner.driver.get("C:/Users/Tkoo/Desktop/Revature_VisualCode/Project2/Project2/project2/src/main/resources/web-pages/login.html");
         
         TestRunner.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         
@@ -51,7 +51,7 @@ public class LoginSteps {
     // Manager log out
     @Given("the manager is on the manager homepage")
     public void the_manager_is_on_the_manager_homepage() {
-        TestRunner.driver.get("File://C:/Users/aflem/OneDrive/Desktop/Project2/project2/src/main/resources/web-pages/manager.html");
+        TestRunner.driver.get("C:/Users/Tkoo/Desktop/Revature_VisualCode/Project2/Project2/project2/src/main/resources/web-pages/manager.html");
     }
     @When("the manager clicks the sign out button")
     public void the_manager_clicks_the_sign_out_button() {
@@ -82,7 +82,7 @@ public class LoginSteps {
     //}
     @Given("the employee is on the login page")
     public void the_employee_is_on_the_login_page() {
-        TestRunner.driver.get("File://C:/Users/aflem/OneDrive/Desktop/Project2/project2/src/main/resources/web-pages/login.html");
+        TestRunner.driver.get("C:/Users/Tkoo/Desktop/Revature_VisualCode/Project2/Project2/project2/src/main/resources/web-pages/login.html");
 
     }
     @When("the employee enters his correct username")
@@ -108,7 +108,7 @@ public class LoginSteps {
     // Requester Log out
     @Given("the employee is on the requester homepage")
     public void the_employee_is_on_the_requester_homepage() {
-        TestRunner.driver.get("File://C:/Users/aflem/OneDrive/Desktop/Project2/project2/src/main/resources/web-pages/requester.html");
+        TestRunner.driver.get("C:/Users/Tkoo/Desktop/Revature_VisualCode/Project2/Project2/project2/src/main/resources/web-pages/requester.html");
 
     }
     @When("the employee clicks the sign out button")
